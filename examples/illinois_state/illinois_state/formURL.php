419a68a054613824b368de6ece65030108fa4513
Bill McMillin <billmcmillin@gmail.com>
2015-09-04 08:21:52 -0400

    update formURL to accomodate multiple copies and locs

--- examples/illinois_state/illinois_state/formURL.php
+++ examples/illinois_state/illinois_state/formURL.php
@@ -7,66 +7,76 @@
 
     if ($_GET['idnum']):
         $id = ($_GET['idnum']);
-    	
-    	$url = 'http://vufind.carli.illinois.edu/vf-isu/Record/AJAX?method=GetHoldings&id=isu_' . $id;
-    	
+      
+      $url = 'http://vufind.carli.illinois.edu/vf-isu/Record/AJAX?method=GetHoldings&id=isu_' . $id;
+      
         getPage($url);
 
     endif;  
 
 function getPage($url){
-	//get the html
+  //get the html
   $web_page = http_get($url, "");
   //make each tr element an element in an array
   $page_array = parse_array($web_page['FILE'], "<tr", "</tr");
   
   //set counter for valid line in html
   $validLine = 0;
+  //set counter for # of items
+  $objNum = 0;
 
   for($x=0; $x<count($page_array); $x++)
-  {
-  	//if the item is a location, pull out the data
-  	if(strpos($page_array[$x], 'col1">Location:') !== false)
-  	{
-  		  $page_array[$x] = preg_replace("/.*<td class=\"col2\">/", "", $page_array[$x]);
-  		  $page_array[$x] = preg_replace("/<.*/", "", $page_array[$x]);
-  		  $td_array[$validLine] = $page_array[$x];
-  	  	//echo $td_array[$validLine].$validLine . "\n";
-   		  $validLine++;
-  	}
-  	//if the item is a call number, pull out the data 	
-  		if(strpos($page_array[$x], 'col1">Call Number') !== false)
-  	{
-  		  $page_array[$x] = preg_replace("/.*<td class=\"col2\">/", "", $page_array[$x]);
-  		  $page_array[$x] = preg_replace("/<.*/", "", $page_array[$x]);
-  		  $td_array[$validLine] = $page_array[$x];
-  	  	//echo $td_array[$validLine].$validLine . "\n";
-  		  $validLine++;
-  	}
-  	//if the item is a copy number pull number. Be careful to include all this so as not to include any notes that contain word copy.
-  	
-  		if(strpos($page_array[$x], 'col1">Copy:&nbsp; </td>') !== false)
-  	{
-  		  $page_array[$x] = preg_replace("/.*<td class=\"col2\">/", "", $page_array[$x]);
-  		  $page_array[$x] = preg_replace("/<.*/", "", $page_array[$x]);
-  		  $td_array[$validLine] = $page_array[$x];
-  	  	//echo $td_array[$validLine].$validLine . "\n";
-  		  $validLine++;
-  	}
-  	
-  	  	//if the item is a status, pull out the data 	
+  {  
+    /*Note: each item must have a location, so when a location is seen, that signals the start of a new object.
+    Since we increment the location object after encountering it, we have to compensate for all other values of that
+    object by subtracting 1 from the object number before multiplying it by 4 and adding the corresponding offset.
+    */
+    //if the item is a location, pull out the data
+    if(strpos($page_array[$x], 'col1">Location:') !== false)
+    {
+        $page_array[$x] = preg_replace("/.*<td class=\"col2\">/", "", $page_array[$x]);
+        $page_array[$x] = preg_replace("/<.*/", "", $page_array[$x]);
+        $td_array[($objNum * 4)] = $page_array[$x];
+        $validLine++;
+        $objNum++;
+    }
+    //if the item is a call number, pull out the data   
+    elseif(strpos($page_array[$x], 'col1">Call Number') !== false)
+    {
+        $page_array[$x] = preg_replace("/.*<td class=\"col2\">/", "", $page_array[$x]);
+        $page_array[$x] = preg_replace("/<.*/", "", $page_array[$x]);
+        $td_array[((($objNum -1) * 4) + 1)] = $page_array[$x];
+        //echo $td_array[$validLine].$validLine . "\n";
+        $validLine++;
+    }
+    //if the item is a copy number pull number. Be careful to include all this so as not to include any notes that contain the word copy.
+    
+    elseif(strpos($page_array[$x], 'col1">Copy:&nbsp; </td>') !== false)
+    {
+        $page_array[$x] = preg_replace("/.*<td class=\"col2\">/", "", $page_array[$x]);
+        $page_array[$x] = preg_replace("/<.*/", "", $page_array[$x]);
+        $td_array[((($objNum -1) * 4) + 2)] = $page_array[$x];
+        //echo $td_array[$validLine].$validLine . "\n";
+        $validLine++;
+    }
+    
+        //if the item is a status, pull out the data  
 
-  		if(strpos($page_array[$x], 'col1">Status:') !== false)
-  	{
-  		  $page_array[$x] = preg_replace("/.*<td class=\"col2\">/", "", $page_array[$x]);
-  		  $page_array[$x] = preg_replace("/<.*/", "", $page_array[$x]);
-  		  $td_array[$validLine] = $page_array[$x];
-  	  //	echo $td_array[$validLine].$validLine . "\n";
-  	    $validLine++;
+    elseif(strpos($page_array[$x], 'col1">Status:') !== false)
+    {
+        $page_array[$x] = preg_replace("/.*<td class=\"col2\">/", "", $page_array[$x]);
+        $page_array[$x] = preg_replace("/<.*/", "", $page_array[$x]);
+        $td_array[((($objNum -1) * 4) + 3)] = $page_array[$x];
+      //  echo $td_array[$validLine].$validLine . "\n";
+        $validLine++;
 
-  	}
+    }
+    else
+    {
+      $td_array[$validLine] = NULL;
+    }
   }
-  	//send to the function that transforms lines into json
+    //send to the function that transforms lines into json
     getBooks($td_array);
 }
 
@@ -86,9 +96,9 @@ function getBooks($td_array) {
   $itemArray = array();
 
   //echo $copynum . "<br />";
-  	for($xy=0; $xy<$numItems; $xy++)
-  		{
-  			$itemNum = $xy + 1;
+    for($xy=0; $xy<$numItems; $xy++)
+      {
+        $itemNum = $xy + 1;
 
         $totalItems[$xy]['itemnum'] = $itemNum;
         $totalItems[$xy]['loc'] = $loc;
@@ -96,12 +106,12 @@ function getBooks($td_array) {
         $totalItems[$xy]['itemavail'] = $itemavail;
 
        
-	  	  }
-	  	  //get the values of the array to prepare to pass
+        }
+        //get the values of the array to prepare to pass
         $json[] = array_values($totalItems);
    
 }
-				//pass json objects to js
+        //pass json objects to js
         echo json_encode($json);
 
 }
